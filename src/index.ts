fa8a3ec32b29d852fb51817e6d14a20b7ef4b677
anovi <mygrib@gmail.com>
2023-02-25 16:21:31 +0100

    feat(core): reading initial state of SM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+/* eslint-disable prettier/prettier */
+import { createEvent, createStore, Event, Store } from 'effector';
+import { reduce } from 'lodash';
+
+export interface FSA_Config {
+  initial: string;
+  states: FSA_States;
+  on?: FSA_Trigger;
+}
+type FSA_StateNode = FSA_Config | { on?: FSA_Trigger };
+
+type FSA_States = { [key: string]: FSA_StateNode };
+
+interface FSA_Trigger {
+  event: Event<any>;
+  target: string;
+}
+
+type StateVaule = string[];
+
+function buildInitialState(data: FSA_Config): string[] {
+  function readStates(states: FSA_States, parent?: string): string[] {
+    const result: string[] = [];
+    return reduce(states, (memo, curr, stateName) => {
+      if ('initial' in curr && stateName === parent) {
+        return memo.concat(readConfig(curr))
+      }
+      return memo;
+    }, result);
+  }
+  function readConfig(config: FSA_Config): string[] {
+    const children = config.states;
+    return [config.initial, ...readStates(children, config.initial)];
+  }
+  return readConfig(data);
+}
+
+function buildStore(initialState: string[]) {
+  return createStore(initialState);
+}
+
+export function createMachine(config: FSA_Config): Store<string[]> {
+  /**
+  * For states 
+  * - create a state tree
+  * For on
+  * - create map of effects
+  */
+  const initialState = buildInitialState(config);
+  const store = buildStore(initialState);
+  return store;
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,60 @@
-import { myPackage } from '../src';
+import { FSA_Config, createMachine } from '../src';
+import { createEvent } from 'effector';
 
 describe('index', () => {
   describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+    it('should return state [upload, idle]', () => {
+      const INIT_UPLOAD = createEvent();
+      const UPLOAD_COMPLETE = createEvent();
+      const INIT_DOWNLOAD = createEvent();
+      const DOWNLOAD_COMPLETE = createEvent();
 
-      const result = myPackage(message);
+      const fileMachine: FSA_Config = {
+        // id: 'file',
+        // type: 'parallel',
+        initial: 'upload',
+        states: {
+          upload: {
+            initial: 'idle',
+            states: {
+              idle: {
+                on: {
+                  event: INIT_UPLOAD,
+                  target: 'pending',
+                },
+              },
+              pending: {
+                on: {
+                  event: UPLOAD_COMPLETE,
+                  target: 'success',
+                },
+              },
+              success: {},
+            },
+          },
+          download: {
+            initial: 'idle',
+            states: {
+              idle: {
+                on: {
+                  event: INIT_DOWNLOAD,
+                  target: 'pending',
+                },
+              },
+              pending: {
+                on: {
+                  event: DOWNLOAD_COMPLETE,
+                  target: 'success',
+                },
+              },
+              success: {},
+            },
+          },
+        },
+      };
 
-      expect(result).toMatch(message);
+      const result = createMachine(fileMachine);
+      expect(result.getState()).toEqual(['upload', 'idle']);
     });
   });
 });
