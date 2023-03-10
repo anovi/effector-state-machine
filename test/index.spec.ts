36e63d99badd1adba88ac7b63d8107f7c1691b7a
anovi <mygrib@gmail.com>
2023-03-10 08:43:10 +0100

    one more spec

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,56 +3,61 @@ import { createEvent } from 'effector';
 
 describe('index', () => {
   describe('myPackage', () => {
-    it('should return state [upload, idle]', () => {
-      const INIT_UPLOAD = createEvent();
-      const UPLOAD_COMPLETE = createEvent();
-      const INIT_DOWNLOAD = createEvent();
-      const DOWNLOAD_COMPLETE = createEvent();
+    const INIT_UPLOAD = createEvent();
+    const UPLOAD_COMPLETE = createEvent();
+    const INIT_DOWNLOAD = createEvent();
+    const DOWNLOAD_COMPLETE = createEvent();
 
-      const fileMachine: FSA_Config = {
-        // id: 'file',
-        // type: 'parallel',
-        initial: 'upload',
-        states: {
-          upload: {
-            initial: 'idle',
-            states: {
-              idle: {
-                on: {
-                  event: INIT_UPLOAD,
-                  target: 'pending',
-                },
+    const fileMachine: FSA_Config = {
+      // id: 'file',
+      // type: 'parallel',
+      initial: 'upload',
+      states: {
+        upload: {
+          initial: 'idle',
+          states: {
+            idle: {
+              on: {
+                event: INIT_UPLOAD,
+                target: 'pending',
               },
-              pending: {
-                on: {
-                  event: UPLOAD_COMPLETE,
-                  target: 'success',
-                },
-              },
-              success: {},
             },
-          },
-          download: {
-            initial: 'idle',
-            states: {
-              idle: {
-                on: {
-                  event: INIT_DOWNLOAD,
-                  target: 'pending',
-                },
+            pending: {
+              on: {
+                event: UPLOAD_COMPLETE,
+                target: 'success',
               },
-              pending: {
-                on: {
-                  event: DOWNLOAD_COMPLETE,
-                  target: 'success',
-                },
-              },
-              success: {},
             },
+            success: {},
           },
         },
-      };
+        download: {
+          initial: 'idle',
+          states: {
+            idle: {
+              on: {
+                event: INIT_DOWNLOAD,
+                target: 'pending',
+              },
+            },
+            pending: {
+              on: {
+                event: DOWNLOAD_COMPLETE,
+                target: 'success',
+              },
+            },
+            success: {},
+          },
+        },
+      },
+    };
 
+    it('should return state [upload, idle]', () => {
+      const result = createMachine(fileMachine);
+      expect(result.getState()).toEqual(['upload', 'idle']);
+    });
+
+    it('should change state to [upload, pending]', () => {
       const result = createMachine(fileMachine);
       expect(result.getState()).toEqual(['upload', 'idle']);
     });
